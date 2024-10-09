e9037dd65bea9ce8e47c5d88c22a10aa47ad0b78
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2024-10-09 19:47:10 +0800

    Update Florido's Info

--- florido_maydelyn.py
+++ florido_maydelyn.py
@@ -2,3 +2,5 @@ print("Name: Maydelyn B. Florido")
 print("Birthday: June 07, 2005")
 print("Gender: Female")
 print("Email: maydelynflorido07@gmail.com")
+
+print("Welcome to the team! let's go! -Riomalos")
