eb550e469ee09b7e0349043d8009aca858227216
jallaine <jallainesiervo143@gmail.com>
2024-10-09 19:37:19 +0800

    add file

---
+++ siervo_jallaine.py
@@ -0,0 +1,11 @@
+# Define the variables
+name = "Jallaine Perpetua G. Siervo"
+birthday = "August 23,2004"
+gender = "Female"
+email = "jallainesiervo143@gmail.com"
+
+# Print the information
+print("Name:", name)
+print("Birthday:", birthday)
+print("Gender:", gender)
+print("Email:", email)
