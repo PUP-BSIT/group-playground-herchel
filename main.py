d8c3ad1198ead73e190a16db501433a917e3169f
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2024-11-27 18:07:51 +0800

    add initial file

---
+++ main.py
@@ -0,0 +1,37 @@
+
+choice = 0
+while choice !=4:
+    print("Herchel Modules")
+    print("1. Riomalos Module")
+    print("2. Zyrrah Module")
+    print("3. Run all")
+    print("4. exit")
+    print(" ")
+    choice = input("Enter your choice: ")
+
+    match choice:
+
+        case '1':
+            from herchel_package import rio_module
+
+            rio_module.numpy_operations()
+        
+        case '2':
+            from herchel_package import zy_module
+
+            zy_module.show_month_calendar()
+            
+            zy_module.is_leap_year()
+        
+        case '3':
+            from herchel_package import zy_module, rio_module
+            
+            print("Numpy Operations")
+            rio_module.numpy_operations()
+
+            print("\nCalendar")
+            zy_module.show_month_calendar()
+            zy_module.is_leap_year()
+        
+        case '4':
+            exit()
--- testO2.py
+++
@@ -1,7 +0,0 @@
-print ("Zyrrah Riomalos")
-print ("September 24, 2005")
-print ("Female")
-print ("zriomalos@gmail.com")
-
-print ("Florido")
-print ("Siervo")
